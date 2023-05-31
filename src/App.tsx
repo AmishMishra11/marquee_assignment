faf5cd5f1c12d196bf9b3f732d72402cddffbb52
AmishMishra11 <amishmishra11@gmail.com>
2023-05-31 18:49:46 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-blue-400 text-4xl">Build in progress...</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
